fbef1a52c376059771ae622278b57f326cb60238
ehddnr177 <ehddnr177@naver.com>
2022-10-17 13:32:15 +0900

    # 변경 사항 - 간단한 테스트용 REST API 추가

--- src/main/java/org/kkukie/jrtsp_gw/controller/MainServiceController.java
+++ src/main/java/org/kkukie/jrtsp_gw/controller/MainServiceController.java
@@ -1,18 +1,37 @@
 package org.kkukie.jrtsp_gw.controller;
 
 import lombok.extern.slf4j.Slf4j;
-import org.kkukie.jrtsp_gw.service.ServiceManager;
-import org.springframework.stereotype.Controller;
+import org.kkukie.jrtsp_gw.service.system.SystemManager;
+import org.kkukie.jrtsp_gw.session.SessionManager;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
-@Controller
+@RestController
+@RequestMapping("/s")
 public class MainServiceController {
 
-    ServiceManager serviceManager;
+    public MainServiceController() {}
 
-    public MainServiceController(ServiceManager serviceManager) {
-        this.serviceManager = serviceManager;
-        this.serviceManager.loop();
+    @GetMapping("/v1/call_count")
+    public String getCallCountV1() {
+        return String.valueOf(SessionManager.getInstance().getCallInfoSize());
+    }
+
+    @GetMapping("/v1/cpu_usage")
+    public String getCpuUsageV1() {
+        return String.valueOf(SystemManager.getInstance().getCpuUsage());
+    }
+
+    @GetMapping("/v1/heap_memory_usage")
+    public String getHeapMemoryUsageV1() {
+        return String.valueOf(SystemManager.getInstance().getHeapMemoryUsage());
+    }
+
+    @GetMapping("/v1/total_thread_count")
+    public String getTotalThreadCountV1() {
+        return String.valueOf(Thread.activeCount());
     }
 
 }
